d5a5201f16d5c9acee5cd19d96687571c0ffcffa
HannahBagtasos <hannahvistabagtasos@gmail.com>
2023-04-14 06:48:21 +0100

    server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-require('dotenv').config() //load our variables from .env
+//require('dotenv').config() //load our variables from .env
 
 const express = require('express') //pull express libarby
 const app = express() //app var to configure server
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true })
 const db = mongoose.connection
 
 db.on('error' , (error) => console.error(error))
-db.on('open' , () => console.log('Connected to Database'))
+db.once('open' , () => console.log('Connected to Database'))
+
+//app.use(express.json()) //let server accept JSON
+
+//const usersRouter = require('./routes/users')
+//app.use('/users', usersRouter)
+
 
 app.listen(3000, () => console.log('Server Started'))
